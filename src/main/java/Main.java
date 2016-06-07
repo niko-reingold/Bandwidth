b2c45b88dbeed3cafdeef223fb433e760f1f7fdd
Niko Reingold <nreingold@nreingold-air.local>
2016-06-07 16:16:12 -0400

    Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,89 +19,86 @@ import static spark.Spark.*;
 
 public class Main {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    port(getHerokuAssignedPort());
+		port(getHerokuAssignedPort());
 
-    authenticate();
+		authenticate();
 
-    staticFileLocation("/public");
-    String layout = "templates/layout.ftl";
+		staticFileLocation("/public");
+		String layout = "templates/layout.ftl";
 
-    get("/", (req, res) -> {
-      HashMap model = new HashMap();
-      model.put("template", "templates/phone.ftl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+		get("/", (req, res) -> {
+			HashMap model = new HashMap();
+			model.put("template", "templates/phone.ftl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
 
-//    post("/phone", (request, response) -> {
-//      
-//      String toNumber = "+1" + request.queryParams("number");
-//      String text = request.queryParams("words");
-//
-//      MustacheFactory mf = new DefaultMustacheFactory();
-//      Mustache mustache = mf.compile("template.mustache");
-//      mustache.execute(new PrintWriter(System.out), new Main()).flush();
-//      
-//      response.type("/bxml/call.xml");
-//
-//      if("call" == request.queryParams("action")){
-//        response.type("/bxml/call.xml");
-//        //outboundCall(number,"+18328627643",text);
-//      } else if ("text" == request.queryParams("action")){
-//        sendText(toNumber,"+18328627643",text);
-//      }
-//      return null;
-//    }); 
-    
-    get("/transfer", (req, res) -> {
-      MustacheFactory mf = new DefaultMustacheFactory();
-      Mustache mustache = mf.compile("/bxml/callForwarding.xml");
-      StringWriter writer = new StringWriter();
-      mustache.execute(writer, this).flush();
+		get("/transfer", (req, res) -> {
+			MustacheFactory mf = new DefaultMustacheFactory();
+			Mustache mustache = mf.compile("/bxml/callForwarding.xml");
+			StringWriter writer = new StringWriter();
+			mustache.execute(writer, new Main()).flush();
 
-      return writer.toString();
-    });
+			return writer.toString();
+		});
 
-    get("/phone", (req, res) -> {
-      HashMap model = new HashMap();
-      model.put("template", "templates/phone.ftl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-   
+		get("/phone", (req, res) -> {
+			HashMap model = new HashMap();
+			model.put("template", "templates/phone.ftl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
 
-  }
+		/*
+		 * post("/phone", (request, response) -> {
+		 * 
+		 * String toNumber = "+1" + request.queryParams("number"); String text =
+		 * request.queryParams("words");
+		 * 
+		 * MustacheFactory mf = new DefaultMustacheFactory(); Mustache mustache
+		 * = mf.compile("template.mustache"); mustache.execute(new
+		 * PrintWriter(System.out), new Main()).flush();
+		 * 
+		 * response.type("/bxml/call.xml");
+		 * 
+		 * if("call" == request.queryParams("action")){
+		 * response.type("/bxml/call.xml");
+		 * //outboundCall(number,"+18328627643",text); } else if ("text" ==
+		 * request.queryParams("action")){
+		 * sendText(toNumber,"+18328627643",text); } return null; });
+		 */
 
-  public static void authenticate(){
-    String userId = "u-72jjash6ldbrtsjvmrsfetq";          //my userId
-        String apiToken = "t-depqhu2y25ut7gsdkussxbq";          //my token
-        String apiSecret = "ajk2odf574li7qvbkz7qtg3fr36wsfnttcpso6y"; //my secret
+	}
 
-        try {
-            BandwidthClient.getInstance().setCredentials(userId, apiToken, apiSecret);
-            // use other resource classes here.
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-  }
-  
-  public static void sendText(String toNumber, String fromNumber, String text){
-    try {
-             Message message = Message.create(toNumber, fromNumber, text);  
-             System.out.println("message:" + message);
-    }
-    catch(Exception e) {
-      e.printStackTrace();
-    }
-  }
+	public static void authenticate() {
+		String userId = "u-72jjash6ldbrtsjvmrsfetq";
+		String apiToken = "t-depqhu2y25ut7gsdkussxbq";
+		String apiSecret = "ajk2odf574li7qvbkz7qtg3fr36wsfnttcpso6y"; 
+																		
 
-  private static int getHerokuAssignedPort() {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    if (processBuilder.environment().get("PORT") != null) {
-      return Integer.parseInt(processBuilder.environment().get("PORT"));
-    }
-    return 4567;
-  }
+		try {
+			BandwidthClient.getInstance().setCredentials(userId, apiToken,
+					apiSecret);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void sendText(String toNumber, String fromNumber, String text) {
+		try {
+			Message message = Message.create(toNumber, fromNumber, text);
+			System.out.println("message:" + message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567;
+	}
 
 }
