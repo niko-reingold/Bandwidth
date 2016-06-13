859fd7dbecb78b36b052b0b8fb86de6d7ed6831a
Niko Reingold <nreingold@nreingold-air.local>
2016-06-13 14:00:41 -0400

    Updated exceptions where possible to be more specific, by using the exceptions in the Java SDK library.a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import com.bandwidth.sdk.model.Message;
 import com.bandwidth.sdk.xml.Response;
 import com.bandwidth.sdk.xml.elements.*;
 
+import com.bandwidth.sdk.exception.XMLMarshallingException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,16 +38,13 @@ public class Main {
 			String text = req.queryParams("words");
 
 			if (req.queryParams("action").equals("call")) {
-				System.out.println("Going to try to make call.");
 				try {
                     String host = "http://" + req.host() + "/callEvents";
-                    System.out.println(host);
 					outboundCall(toNumber, fromNumber, host, text);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			} else {
-				System.out.println("Going to try to send text.");
 				sendText(toNumber, fromNumber, text);
 			}
 			
@@ -61,9 +60,6 @@ public class Main {
 
             String bxml = "";
 
-            System.out.println(text);
-            System.out.println(event);
-
             if(event.equals("answer")){
                 try {
                     Response response = new Response();
@@ -75,13 +71,11 @@ public class Main {
                     response.add(hangup);
                     bxml = response.toXml();
 
-                    System.out.println("Made bxml response");
-                    System.out.println(bxml);
-
                     res.type("text/xml");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                } catch (XMLMarshallingException e) {
+					System.out.println(bxml);
+					System.out.println(e);
+				}
                 return bxml;
             } else {
                 res.status(200);
@@ -90,7 +84,6 @@ public class Main {
         });
 
 		get("/transferText", (req, res) -> {
-			System.out.println("Transferring text");
 
 			String bxml = "";
 			try {
@@ -104,24 +97,19 @@ public class Main {
 
 				bxml = response.toXml();
 
-				System.out.println("Made bxml response");
-				System.out.println(bxml);
-
 				res.type("text/xml");
-			} catch (Exception e) {
-				e.printStackTrace();
+			} catch (XMLMarshallingException e) {
+				System.out.println(bxml);
+				System.out.println(e);
 			}
 			return bxml;
 		});
 
         get("/transfer", (req, res) -> {
 
-            System.out.println("In transfer");
-
             String bxml = "";
             //           String callerID = req.queryParams("callId");
             if(req.queryParams("eventType").equals("answer")){
-                System.out.println("recieving call");
                 try {
                     Response response = new Response();
 
@@ -134,13 +122,11 @@ public class Main {
 
                     bxml = response.toXml();
 
-                    System.out.println("Made bxml response");
-                    System.out.println(bxml);
-
                     res.type("text/xml");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                } catch (XMLMarshallingException e) {
+					System.out.println(bxml);
+					System.out.println(e);
+				}
                 return bxml;
             } else {
                 res.status(200);
@@ -166,11 +152,6 @@ public class Main {
 	public static void outboundCall(String toNumber, String fromNumber, String callbackURL,
 			String text) throws Exception {
 
-		System.out.println("Inside call function");
-		System.out.println("toNumber: " + toNumber);
-		System.out.println("fromNumber: " + fromNumber);
-		System.out.println("Message: " + text);
-
 		final Map<String, Object> params = new HashMap<String, Object>();
 		params.put("to", toNumber);
 		params.put("from", fromNumber);
@@ -179,14 +160,9 @@ public class Main {
 		params.put("callbackHttpMethod", "GET");
 
 		Call.create(params);
-		System.out.println("Call created");
 	}
 
 	public static void sendText(String toNumber, String fromNumber, String text) {
-		System.out.println("Inside text function");
-		System.out.println("toNumber: " + toNumber);
-		System.out.println("fromNumber: " + fromNumber);
-		System.out.println("Message: " + text);
 		try {
 			Message message = Message.create(toNumber, fromNumber, text);
 			System.out.println("Sent Message");
