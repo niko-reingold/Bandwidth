fdd9e53cafc827d52dae59984183037baf440a68
Niko Reingold <nreingold@nreingold-air.local>
2016-06-21 13:46:59 -0400

    Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,48 +14,48 @@ import static spark.Spark.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		port(getHerokuAssignedPort());
+        port(getHerokuAssignedPort());
 
-		authenticate();
-		String fromNumber = System.getenv().get("PHONE_NUMBER");
+        authenticate();
+        String fromNumber = System.getenv().get("PHONE_NUMBER");
         String[] voicemails = new String[5];
         int vmCounter = 0;
 
-		staticFileLocation("/public");
-		String layout = "templates/layout.ftl";
+        staticFileLocation("/public");
+        String layout = "templates/layout.ftl";
 
-		get("/", (req, res) -> {
-			HashMap model = new HashMap();
-			model.put("template", "templates/phone.ftl");
-			model.put("fromNumber", fromNumber);
-			return new ModelAndView(model, layout);
-		}, new VelocityTemplateEngine());
+        get("/", (req, res) -> {
+            HashMap model = new HashMap();
+            model.put("template", "templates/phone.ftl");
+            model.put("fromNumber", fromNumber);
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
-		get("/phone", (req, res) -> {
-			String toNumber = "+1" + req.queryParams("number");
-			String text = req.queryParams("words");
+        get("/phone", (req, res) -> {
+            String toNumber = "+1" + req.queryParams("number");
+            String text = req.queryParams("words");
 
-			if (req.queryParams("action").equals("call")) {
-				System.out.println("Going to try to make call.");
-				try {
+            if (req.queryParams("action").equals("call")) {
+                System.out.println("Going to try to make call.");
+                try {
                     String host = "http://" + req.host() + "/callEvents";
                     System.out.println(host);
-					outboundCall(toNumber, fromNumber, host, text);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			} else {
-				System.out.println("Going to try to send text.");
-				sendText(toNumber, fromNumber, text);
-			}
-			
-			HashMap model = new HashMap();
-			model.put("template", "templates/phone.ftl");
-			model.put("fromNumber", fromNumber);
-			return new ModelAndView(model, layout);
-		}, new VelocityTemplateEngine());
+                    outboundCall(toNumber, fromNumber, host, text);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Going to try to send text.");
+                sendText(toNumber, fromNumber, text);
+            }
+
+            HashMap model = new HashMap();
+            model.put("template", "templates/phone.ftl");
+            model.put("fromNumber", fromNumber);
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
         get("/callEvents", (req, res) -> {
             String text = req.queryParams("tag");
@@ -66,7 +66,7 @@ public class Main {
             System.out.println(text);
             System.out.println(event);
 
-            if(event.equals("answer")){
+            if (event.equals("answer")) {
                 try {
                     Response response = new Response();
 
@@ -91,36 +91,36 @@ public class Main {
             }
         });
 
-		get("/transferText", (req, res) -> {
-			System.out.println("Transferring text");
+        get("/transferText", (req, res) -> {
+            System.out.println("Transferring text");
 
-			String bxml = "";
-			try {
-				Response response = new Response();
+            String bxml = "";
+            try {
+                Response response = new Response();
 
-				String originalNumber = req.queryParams("from");
-				String originalText = req.queryParams("text");
-				SendMessage sendMessage = new SendMessage(fromNumber, "+19196705750", originalNumber + ": " + originalText);
+                String originalNumber = req.queryParams("from");
+                String originalText = req.queryParams("text");
+                SendMessage sendMessage = new SendMessage(fromNumber, "+19196705750", originalNumber + ": " + originalText);
 
-				response.add(sendMessage);
+                response.add(sendMessage);
 
-				bxml = response.toXml();
+                bxml = response.toXml();
 
-				System.out.println("Made bxml response");
-				System.out.println(bxml);
+                System.out.println("Made bxml response");
+                System.out.println(bxml);
 
-				res.type("text/xml");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return bxml;
-		});
+                res.type("text/xml");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bxml;
+        });
 
-		get("/voicemail", (req, res) -> {
-			System.out.println("In voicemail");
+        get("/voicemail", (req, res) -> {
+            System.out.println("In voicemail");
 
-			String bxml = "";
-            if(req.queryParams("eventType").equals("answer")){
+            String bxml = "";
+            if (req.queryParams("eventType").equals("answer")) {
                 try {
                     Response response = new Response();
 
@@ -139,7 +139,7 @@ public class Main {
                     System.out.println(bxml);
 
                     res.type("text/xml");
-                } catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
                 return bxml;
@@ -147,15 +147,15 @@ public class Main {
                 res.status(200);
                 return res;
             }
-		});
+        });
 
-        get("/voicemail/transfer", (req, res) -> {
+        get("/transfer", (req, res) -> {
 
             System.out.println("In transfer");
 
             String bxml = "";
             //           String callerID = req.queryParams("callId");
-            if(req.queryParams("dtmfDigit").equals("2")){
+            if (req.queryParams("dtmfDigit").equals("2")) {
                 System.out.println("pressed 2");
                 try {
                     Response response = new Response();
@@ -206,59 +206,59 @@ public class Main {
 //            return new ModelAndView(model, layout);
 //        }, new VelocityTemplateEngine());
 //
-	}
+    }
 
-	public static void authenticate() {
-		String userId = System.getenv().get("BANDWIDTH_USER_ID");
-		String apiToken = System.getenv().get("BANDWIDTH_API_TOKEN");
-		String apiSecret = System.getenv().get("BANDWIDTH_API_SECRET");
+    public static void authenticate() {
+        String userId = System.getenv().get("BANDWIDTH_USER_ID");
+        String apiToken = System.getenv().get("BANDWIDTH_API_TOKEN");
+        String apiSecret = System.getenv().get("BANDWIDTH_API_SECRET");
 
-		try {
-			BandwidthClient.getInstance().setCredentials(userId, apiToken,
-					apiSecret);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            BandwidthClient.getInstance().setCredentials(userId, apiToken,
+                    apiSecret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void outboundCall(String toNumber, String fromNumber, String callbackURL,
-			String text) throws Exception {
+    public static void outboundCall(String toNumber, String fromNumber, String callbackURL,
+                                    String text) throws Exception {
 
-		System.out.println("Inside call function");
-		System.out.println("toNumber: " + toNumber);
-		System.out.println("fromNumber: " + fromNumber);
-		System.out.println("Message: " + text);
+        System.out.println("Inside call function");
+        System.out.println("toNumber: " + toNumber);
+        System.out.println("fromNumber: " + fromNumber);
+        System.out.println("Message: " + text);
 
-		final Map<String, Object> params = new HashMap<String, Object>();
-		params.put("to", toNumber);
-		params.put("from", fromNumber);
-		params.put("callbackUrl", callbackURL);
-		params.put("tag", text);
-		params.put("callbackHttpMethod", "GET");
+        final Map<String, Object> params = new HashMap<String, Object>();
+        params.put("to", toNumber);
+        params.put("from", fromNumber);
+        params.put("callbackUrl", callbackURL);
+        params.put("tag", text);
+        params.put("callbackHttpMethod", "GET");
 
-		Call.create(params);
-		System.out.println("Call created");
-	}
+        Call.create(params);
+        System.out.println("Call created");
+    }
 
-	public static void sendText(String toNumber, String fromNumber, String text) {
-		System.out.println("Inside text function");
-		System.out.println("toNumber: " + toNumber);
-		System.out.println("fromNumber: " + fromNumber);
-		System.out.println("Message: " + text);
-		try {
-			Message message = Message.create(toNumber, fromNumber, text);
-			System.out.println("Sent Message");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void sendText(String toNumber, String fromNumber, String text) {
+        System.out.println("Inside text function");
+        System.out.println("toNumber: " + toNumber);
+        System.out.println("fromNumber: " + fromNumber);
+        System.out.println("Message: " + text);
+        try {
+            Message message = Message.create(toNumber, fromNumber, text);
+            System.out.println("Sent Message");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private static int getHerokuAssignedPort() {
-		ProcessBuilder processBuilder = new ProcessBuilder();
-		if (processBuilder.environment().get("PORT") != null) {
-			return Integer.parseInt(processBuilder.environment().get("PORT"));
-		}
-		return 4567;
-	}
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
 
 }
