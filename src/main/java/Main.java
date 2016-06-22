1b1d6561abbf0b2495f206b209b7c6a7f78e1de3
Niko Reingold <nreingold@nreingold-air.local>
2016-06-22 08:53:36 -0400

    Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -154,6 +154,9 @@ public class Main {
             System.out.println("In transfer");
             System.out.println(req.queryParams());
             System.out.println(req.queryParams("eventType"));
+            if(req.queryParams("eventType").equals("hangup")){
+                return null;
+            }
             String bxml = "";
             //           String callerID = req.queryParams("callId");
             try{
